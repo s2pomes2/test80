c204bf1d1d8f496bbd609c2a94cf3bc6a9588b89
hanbee <s2pomes2@naver.com>
2016-04-20 15:12:32 +0900

    '.'

--- test01/src/test01/Hello2.java
+++ test01/src/test01/Hello2.java
@@ -1,10 +1,10 @@
 package test01;
 
-public class Hello {
+public class Hello2 {
 
   public static void main(String[] args) {
     System.out.println("Hello!");
-
+    System.out.println("Hello,Hello!");
   }
 
 }
